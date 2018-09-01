ce2adb2bf0bd31fe4f5d93186d4a57799a3865ce
xozen <42662848+xozen@users.noreply.github.com>
2018-09-02 01:08:47 +0900

    Add files via upload
    windowcopen 1.0 버전입니다.
    제가 실무에 애용중입니다. ^^

---
+++ windowcopen.js
@@ -0,0 +1,25 @@
+/*
+
+WindowCOpen 1.0
+http://github.com/xozen/windowcopen
+
+*/
+function windowcopen(url, title, options) {
+
+    var option_nosize = ''
+    options_arr = options.split(',')
+    for (i = 0; (i + 1) <= options_arr.length; i++) {
+        option = options_arr[i]
+        option_arr = option.split('=')
+        var fld = option_arr[0]
+        var val = option_arr[1]
+        if (fld == 'width') var ww = val
+        else if (fld == 'height') var wh = val
+        else option_nosize += ',' + fld + '=' + val
+    }
+
+    var left = (screen.width / 2) - (ww / 2)
+    var top = (screen.height / 2) - (wh / 2) - 50
+    return window.open(url, title, 'width=' + ww + ', height=' + wh + ', top=' + top + ', left=' + left + option_nosize)
+
+}
